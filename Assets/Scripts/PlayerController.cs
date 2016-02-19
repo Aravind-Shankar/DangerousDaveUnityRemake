c94b4d5a3a9c99abac6587e8bb245abecd0ec8c0
Aravind Shankar <shankaravind167@gmail.com>
2016-02-19 19:08:53 +0530

    Added the left-out files

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private bool canJump = false;
+	private Transform groundCheck;
+
+	public float moveForce = 5f;
+	public float jumpForce = 50f;
+
+	// Use this for initialization
+	void Start () {
+		groundCheck = transform.Find ("ground_check");
+	}
+
+	void Update() {
+		groundCheck.transform.rotation.eulerAngles = Vector3.zero;
+		canJump = (Input.GetButtonDown("Jump") &&
+		        Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground Layer"))
+		        );
+	}
+	
+	void FixedUpdate () {
+		float horiz = Input.GetAxis ("Horizontal") * moveForce, vert = (canJump) ? jumpForce : 0;
+		this.GetComponent<Rigidbody2D> ().AddForce (new Vector2 (horiz, vert));
+	}
+}
