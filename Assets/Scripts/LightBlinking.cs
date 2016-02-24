b87a3890cd45017b30523780711f69e0e97443f0
Aravind Shankar <shankaravind167@gmail.com>
2016-02-24 22:09:04 +0530

    Radar bug fixed; made radar player backlit; renamed radar as DaveNav for now

---
+++ Assets/Scripts/LightBlinking.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightBlinking : MonoBehaviour {
+	
+	public float brightTimeSeconds = 0.5f;
+	public float darkTimeSeconds = 0.5f;
+	
+	private Light light;
+	
+	// Use this for initialization
+	void Start () {
+		light = GetComponent<Light> ();
+		StartCoroutine (BlinkLight ());
+	}
+	
+	void Update() {
+	}
+	
+	IEnumerator BlinkLight() {
+		while (true) {
+			light.intensity = 8;
+			yield return new WaitForSeconds(brightTimeSeconds);
+			light.intensity = 1;
+			yield return new WaitForSeconds(darkTimeSeconds);
+		}
+	}
+	
+}
